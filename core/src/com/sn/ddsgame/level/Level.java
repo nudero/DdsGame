c01bd862ea984f2d460485a250f7a518541f8bb6
haibo yin <haiboyin@haibos-MacBook-Pro.local>
2015-08-31 20:22:55 +0800

    1

--- core/src/com/sn/ddsgame/level/Level.java
+++ core/src/com/sn/ddsgame/level/Level.java
@@ -1,48 +1,34 @@
 package com.sn.ddsgame.level;
 
 public class Level {
-	
-	public static final int NONE = -2;
+	public static final int END = -3;
+	public static final int NO_OUTPUT = -2;
 	public static final int RANDOM = -1;
 	
-	public LevelData data = null;
-	private int stepIndex = 0;
-	private float stepTime = 0;
-	private boolean over = false;
+	private LevelData data;
+	private int round;
+	private int index;
+	private 
 	
 	public Level(LevelData data) {
 		this.data = data;
+		round = -1;
+		index = -1;
+		toRound();
 	}
 	
-	public void start() {
-		over = false;
-		stepIndex = 0;
-		stepTime = data.roundTime;
-	}
-	
-	public int step(float dt) {
-		stepTime -= dt;
-		if (stepTime > 0) {
-			return NONE;
+	private boolean toRound() {
+		round++;
+		index = 0;
+		if (round >= data.roundNum) {
+			return true;
 		}
-		PosTime pt = data.postimes.get(stepIndex++);
-		stepTime = pt.time;
+		return false;
+	}
+	
+	public int step(float delta) {
 		
-		if (stepIndex >= data.postimes.size()) {
-			over = true;
-		}
-		return pt.pos;
+		
+		return 0;
 	}
-
-	public boolean isOver() {
-		return over;
-	}
-
-//	public int getNext() {
-//		if (stepIndex >= data.postimes.size()) {
-//			return NONE;
-//		}
-//		PosTime pt = data.postimes.get(stepIndex);
-//		return pt.pos;
-//	}
 }
